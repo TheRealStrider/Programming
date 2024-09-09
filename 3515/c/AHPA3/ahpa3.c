35abb83bf04a5d793406272759200bdb1e76b42b
Strider <starwars3805@gmail.com>
2024-09-08 21:46:15 -0400

    Load all >:)

---
+++ 3515/c/AHPA3/ahpa3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+enum flavors {vanilla, chocolate, mint, strawberry};
+
+int main() {
+    enum flavors v = vanilla;
+    enum flavors c = chocolate;
+    enum flavors m = mint;
+    enum flavors s = strawberry;
+
+    float cornerOneSales[4];
+    float cornerTwoSales[4];
+
+    return 0;
+}
